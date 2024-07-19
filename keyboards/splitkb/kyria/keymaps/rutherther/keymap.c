6042f90b9f42f44852925a083172127f457fb913
Rutherther <rutherther@proton.me>
2024-07-19 21:08:21 +0200

    feat: add delete key, TTY key

--- keyboards/splitkb/kyria/keymaps/rutherther/keymap.c
+++ keyboards/splitkb/kyria/keymaps/rutherther/keymap.c
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     SUP_TAB,  KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,                                                        KC_Y,     KC_U,       KC_I,     KC_O,     KC_P,     SUP_BSLS,
     CTL_ESC,  SYM_A,    KC_S,     KC_D,     KC_F,      NUM_G,                                                       KC_H,     KC_J,       KC_K,     KC_L,     SYM_SCLN, CTL_QUOT,
-    OSM_LSFT, KC_Z,     KC_X,     KC_C,     NAV_V,     KC_B,     GAMES,    CTRL,               FKEYS,     _______,  KC_N,     GUI_M,      KC_COMM,  KC_DOT,   KC_SLSH,  OSM_RSFT,
-                                  QK_LOCK,  CZ_ACUTED, QK_REP,   ALT_SPC,  CZ_ACUTED,          CZ_CARETED,ALT_ENT,  KC_BSPC,  CZ_CARETED, KC_APP
+    OSM_LSFT, KC_Z,     KC_X,     KC_C,     NAV_V,     KC_B,     GAMES,    CTRL,               FKEYS,     TTY,      KC_N,     GUI_M,      KC_COMM,  KC_DOT,   KC_SLSH,  OSM_RSFT,
+                                  QK_LOCK,  _______,   QK_REP,   ALT_SPC,  CZ_ACUTED,          CZ_CARETED,ALT_ENT,  KC_BSPC,  KC_DEL,     KC_APP
   ),
 
   [_PLAIN] = LAYOUT(
